08e103efb84320ce35f1e95c87efcff3a451e2d3
Rhet0rical <meddakee2000@gmail.com>
2023-04-22 16:51:30 -0400

    Update To 2.0

--- README.md
+++ README.md
@@ -1,30 +1,35 @@
-# Rhet0rical's Install Script (**ARCHIVED**)
-> This was archived because it's being remastered. You can check my repo list for the *setup-script*
-
-If your running Windows Version **10 or 11**, it should run right out of the box after extracting the root file since it utilizes batch. The script will be updated pretty often, or as often as i find new tools to use. It completes this task by utilizing the ```start``` command that opens the browser to a URL. This entire script basically works by downloading files using direct download links and runnning them in a browser.
+# Rhet0rical's Install Script - *Version 2.0*
+If your running Windows Version **10 or 11**, it should run right out of the box after extracting the root file since it utilizes batch. It completes this task by utilizing the ```start``` command that opens the **default browser** to a URL.
 
 When it runs, it'll run a ```pause``` statement before starting, so it won't immediately install everything until you tell it to.
 
 ## Editing
 You can add your own files to download by just adding... \
 ```start "{preferred browser here, leave blank for default}" "{download url}"``` \
-```start "" "https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x86"```
 on a new line. You can also remove programs too by just deleting the line with the link.
 
-## Details
-As of right now, it installs...
+## Install List
+As of right version 2.0, it installs...
 
-* Visual Studio Code
-* GitHub Desktop
-* WinRar
-* NodeJS Runtime
-* Visual C++ Redistributable Runtimes AIO Repack (courtesy of MajorGeeks)
-* GDevelop 5
-* Steam
-* BattleNet Launcher
-* Epic Games Launcher
-* Discord
-* Google Chrome
-* Google Drive Desktop
-* Spotify
-* NVIDIA GeForce Experience
+### Development Tools
+* Visual Studio Code - [**Link**](https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user)
+
+* GitHub Desktop - [**Link**](https://central.github.com/deployments/desktop/desktop/latest/win32)
+
+* NodeJS - [**Link**](https://nodejs.org/dist/v18.16.0/node-v18.16.0-x64.msi)
+
+* GDevelop [**Link**](https://gdevelop.io/download/windows)
+
+### Chrome Tools
+* Google Chrome - [**Link**](https://www.google.com/chrome/thank-you.html?installdataindex=empty&statcb=1&defaultbrowser=0&brand=QMKX#)
+
+* Google Drive For Desktop - [**Link**](https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe)
+
+### Game & Social Platforms
+* Steam - [**Link**](https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe)
+
+* BattleNet - [**Link**](https://www.battle.net/download/getInstallerForGame?os=win&gameProgram=BATTLENET_APP&version=Live&id=1169942068.1682195073&_gl=1*1ytqb4c*_ga*MTE2OTk0MjA2OC4xNjgyMTk1MDcz*_ga_VYKNV7C0S3*MTY4MjE5NTA3Mi4xLjEuMTY4MjE5NTA4MS41MS4wLjA)
+
+* Discord - [**Link**](https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x86)
+
+* Spotify - [**Link**](https://download.scdn.co/SpotifySetup.exe)
--- Script (x86).bat
+++ Script (x86).bat
@@ -1,14 +1,15 @@
 @echo off
 
-echo Rhet0rical's Install Script Version 1.1
-echo Using this script will install various items by using your default browser
-echo to download various programs.
-echo:
+echo Rhet0rical's Install Script - Version 2.0
+echo This script will utilize your default browser to install various applications by
+echo loading their respective webpages. You can check what applications it installs by
+echo visiting the GitHub. https://github.com/TheRhet0rical/setup-script
 
+echo:
 pause
 
 echo:
-echo Downloading Development Materials Such As...
+echo Installing Development Tools Such As...
 
 echo Visual Studio Code...
 start "" "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user"
@@ -16,48 +17,36 @@ start "" "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-u
 echo GitHub Desktop...
 start "" "https://central.github.com/deployments/desktop/desktop/latest/win32"
 
-echo WinRar...
-start "" "https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-621.exe"
-
 echo NodeJS...
-start "" "https://nodejs.org/dist/v18.15.0/node-v18.15.0-x64.msi"
+start "" "https://nodejs.org/dist/v18.16.0/node-v18.16.0-x64.msi"
 
-echo Visual C++ Redistributable Runtimes AIO Repack...
-start "" "https://www.majorgeeks.com/index.php?ct=files&action=download&"
-
-echo GDevelop 5...
+echo GDevelop...
 start "" "https://gdevelop.io/download/windows"
 
 echo:
-echo Downloading Game and Social Networks Such As...
+echo Installing Chrome Tools Such As...
+
+echo Google Chrome...
+start "" "https://www.google.com/chrome/thank-you.html?installdataindex=empty&statcb=1&defaultbrowser=0&brand=QMKX#"
+
+echo Google Drive For Desktop...
+start "" "https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe"
+
+echo:
+echo Downloading Game & Social Platforms Such As...
 
 echo Steam...
-start "" "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe"
+start "" "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe"
 
-echo BattleNet Launcher...
-start "" "https://www.battle.net/download/getInstallerForGame?os=win&gameProgram=BATTLENET_APP&version=Live&id=1458664938.1678749340&_gl=1*156n8i7*_ga*MTQ1ODY2NDkzOC4xNjc4NzQ5MzQw*_ga_VYKNV7C0S3*MTY4MDkyNTg4MS4zLjEuMTY4MDkyNTkxNC4yNy4wLjA."
-
-echo Epic Games Launcher...
-start "" "https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi"
+echo BattleNet...
+start "" "https://www.battle.net/download/getInstallerForGame?os=win&gameProgram=BATTLENET_APP&version=Live&id=1169942068.1682195073&_gl=1*1ytqb4c*_ga*MTE2OTk0MjA2OC4xNjgyMTk1MDcz*_ga_VYKNV7C0S3*MTY4MjE5NTA3Mi4xLjEuMTY4MjE5NTA4MS41MS4wLjA."
 
 echo Discord...
 start "" "https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x86"
 
-echo:
-echo Downloading Misc Materials Such As...
-
-echo Google Chrome...
-start "" "https://www.google.com/chrome/thank-you.html?statcb=1&installdataindex=empty&defaultbrowser=0#"
-
-echo Google Drive Desktop...
-start "" "https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe"
-
 echo Spotify...
 start "" "https://download.scdn.co/SpotifySetup.exe"
 
-echo NVIDIA GeForce Experience...
-start "" "https://us.download.nvidia.com/GFE/GFEClient/3.27.0.112/GeForce_Experience_v3.27.0.112.exe"
-
 echo:
-echo The installer finished installing the programs listed above. You can exit this program at any time.
+echo Finished Installing The Applications. This Application Will Close.
 pause
